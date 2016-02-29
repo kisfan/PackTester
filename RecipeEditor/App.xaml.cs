0a81accfa878f926ba442d036a35b532d731fb04
kisfan <kisfan@pmparts.com>
2016-02-29 12:36:52 -0600

    kyle add recipeeditor

--- RecipeEditor/App.xaml.cs
+++ RecipeEditor/App.xaml.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
-namespace PackTesterInterface
+namespace RecipeEditor
 {
     /// <summary>
     /// Interaction logic for App.xaml
